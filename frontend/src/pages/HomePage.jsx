751b38246ceb977940073843fe19a751bc59a924
Raamanjal <gangwarraamanjal@gmail.com>
2026-03-03 23:36:13 +0530

    created home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import RateLimitUI from '../components/RateLimitUI';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import axios from 'axios';
+import NoteCard from '../components/NoteCard';
 
 const HomePage = () => {
      const [isRateLimited,setIsRateLimited] = useState(false);
@@ -35,10 +36,22 @@ const HomePage = () => {
      }, []);
 
   return (
-    <div className='min-h-screen'>
+    <div className='min-h-screen bg-black'>
         <Navbar/>
 
         {isRateLimited && <RateLimitUI/>}
+
+        <div className='max-w-7xl mx-auto p-4 mt-6'>
+            {loading && <div className='text-center text-primary py-10'>Loading Notes...</div>}
+
+            {notes.length>0 && !isRateLimited &&(
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+                    {notes.map(note =>(
+                       <NoteCard key={note._id} note={note} />
+                    ))}
+                </div>   
+            )}
+        </div>
     </div>
   )
 }
