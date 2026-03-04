8cc6f18c46ad046746e0773f0fd78ba35999f1a4
Raamanjal <gangwarraamanjal@gmail.com>
2026-03-04 20:48:51 +0530

    project finished

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,59 +1,60 @@
-import React from 'react'
-import toast from 'react-hot-toast'
-import Navbar from '../components/Navbar'
-import RateLimitUI from '../components/RateLimitUI';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import axios from 'axios';
-import NoteCard from '../components/NoteCard';
+import { useState } from "react";
+import Navbar from "../components/Navbar";
+import RateLimitedUI from "../components/RateLimitUI";
+import { useEffect } from "react";
+import api from "../lib/axios";
+import toast from "react-hot-toast";
+import NoteCard from "../components/NoteCard";
+import NotesNotFound from "../components/NotesNotFound";
 
 const HomePage = () => {
-     const [isRateLimited,setIsRateLimited] = useState(false);
-     const [notes,setNotes] = useState([]);
-     const [loading,setLoading] = useState(true)
+  const [isRateLimited, setIsRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-     useEffect(()=>{
-        const fetchNotes = async () =>{
-            try{
-                const res = await axios.get("http://localhost:4000/api/notes");
-                console.log(res.data);
-                setNotes(res.data);
-                setIsRateLimited(false);
-            } catch (error){
-                console.log("Error Fetching notes");
-                if(error.response?.status === 429){
-                    setIsRateLimited(true);
-                }
-                else{
-                    toast.error("failed to load notes");
-                }
-            }
-            finally{
-                setLoading(false);
-            }
-        };
-        fetchNotes();
-     }, []);
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await api.get("/notes");
+        console.log(res.data);
+        setNotes(res.data);
+        setIsRateLimited(false);
+      } catch (error) {
+        console.log("Error fetching notes");
+        console.log(error.response);
+        if (error.response?.status === 429) {
+          setIsRateLimited(true);
+        } else {
+          toast.error("Failed to load notes");
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchNotes();
+  }, []);
 
   return (
-    <div className='min-h-screen bg-black'>
-        <Navbar/>
+    <div className="min-h-screen bg-black">
+      <Navbar />
 
-        {isRateLimited && <RateLimitUI/>}
+      {isRateLimited && <RateLimitedUI />}
 
-        <div className='max-w-7xl mx-auto p-4 mt-6'>
-            {loading && <div className='text-center text-primary py-10'>Loading Notes...</div>}
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
 
-            {notes.length>0 && !isRateLimited &&(
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-                    {notes.map(note =>(
-                       <NoteCard key={note._id} note={note} />
-                    ))}
-                </div>   
-            )}
-        </div>
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
+
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} setNotes={setNotes} />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
-  )
-}
-
-export default HomePage
+  );
+};
+export default HomePage;
