57ba6c77189e0d0b529dce7a7057206e28360069
Raamanjal <gangwarraamanjal@gmail.com>
2026-03-04 21:38:25 +0530

    ready for prod

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-
+import path from "path"
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -10,11 +10,16 @@ import rateLimiter from "./middleware/rateLimiter.js";
 const app = express();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use(cors({
-    origin: "http://localhost:5173",
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
 }
-));
+
 app.use(express.json());
 
 app.use((req,res,next)=>{
@@ -24,6 +29,14 @@ app.use((req,res,next)=>{
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 
 
